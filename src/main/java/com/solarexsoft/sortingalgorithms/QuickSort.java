deee44a9fe94d4ac65fc1b7e108e495a3515b9f6
Solarex <rh.hou.work@gmail.com>
2019-10-05 17:02:19 +0800

    hoare quick sort

--- src/main/java/com/solarexsoft/sortingalgorithms/QuickSort.java
+++ src/main/java/com/solarexsoft/sortingalgorithms/QuickSort.java
@@ -30,4 +30,23 @@ public class QuickSort extends SortingAlgorithms {
             lomutoSort(arr, k+1, high);
         }
     }
+
+    private int hoarePartition(int[] arr, int low, int high) {
+        int pivot = arr[low + (high-low)/2];
+        int i = low, j = high;
+        while (true) {
+            while (arr[i] < pivot) ++i;
+            while (arr[j] > pivot) --j;
+            if (i >= j) return j;
+            swap(arr, i++, j--);
+        }
+    }
+
+    private void hoareSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int k = hoarePartition(arr, low, high);
+            hoareSort(arr, low, k);
+            hoareSort(arr, k+1, high);
+        }
+    }
 }
