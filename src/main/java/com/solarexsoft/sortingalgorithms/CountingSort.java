633680a2b8b87b5ad634bdaef0d1f8b8e1fe4bb9
Solarex <rh.hou.work@gmail.com>
2019-10-06 10:05:48 +0800

    counting sort

--- src/main/java/com/solarexsoft/sortingalgorithms/CountingSort.java
+++ src/main/java/com/solarexsoft/sortingalgorithms/CountingSort.java
@@ -42,5 +42,34 @@ public class CountingSort extends SortingAlgorithms {
         System.arraycopy(tmp, 0, arr, start, end - start);
     }
 
+    // indexes 最后存储的是排序后，相同数字在结果数组中的结束位置，相同数字会依次向前填充
+    // Time: O(n+k) Space: O(n+k)
+    public void sortRight2Left(int arr[], int start, int end) {
+        int max = arr[start], min = arr[start];
+        for (int i = start + 1; i < end; i++) {
+            if (min > arr[i]) min = arr[i];
+            if (max < arr[i]) max = arr[i];
+        }
+
+        int k = max - min;
+        int[] indexes = new int[k+1];
+        for (int i = start; i < end; i++) {
+            ++indexes[arr[i] - min]; // 统计每个数字出现的次数
+        }
+
+        --indexes[0];
+        for (int i = 1; i <= k; i++) {
+            indexes[i] = indexes[i] + indexes[i-1];
+        }
+
+        int[] tmp = new int[end - start];
+        for (int i = end - 1; i >= start; i--) {
+            int idx = indexes[arr[i] - min];
+            tmp[idx] = arr[i];
+            --indexes[arr[i] - min];
+        }
+
+        System.arraycopy(tmp, 0, arr, 0, end - start);
+    }
 
 }
